b79b1e3055d6072c60dd26976f75b0c2ca8b917a
Ricardo Santana <rsantana@kenos.com.br>
2019-12-12 14:50:33 -0300

    fix build files

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ configure () {
 
 	# Ask for iDempiere folder
 	while true; do
-	read -e -p "Select the absolute location of iDempiere project [$PWD/../iDempiere6.2]: " IDEMPIERE_FOLDER
+	read -e -p "Select the absolute location of iDempiere project [$PWD/../idempiere]: " IDEMPIERE_FOLDER
 	case $IDEMPIERE_FOLDER in
-		"" ) if [ ! -f "$PWD/../iDempiere6.2/org.idempiere.parent/pom.xml" ]; then echo "Error:	File pom.xml not found => $IDEMPIERE_FOLDER/org.idempiere.parent/pom.xml"; else IDEMPIERE_FOLDER="$PWD/../iDempiere6.2"; break; fi;;
+		"" ) if [ ! -f "$PWD/../idempiere/org.idempiere.parent/pom.xml" ]; then echo "Error:	File pom.xml not found => $IDEMPIERE_FOLDER/org.idempiere.parent/pom.xml"; else IDEMPIERE_FOLDER="$PWD/../idempiere"; break; fi;;
 		* ) if [ ! -f "$IDEMPIERE_FOLDER/org.idempiere.parent/pom.xml" ]; then echo "Error:	File pom.xml not found => $IDEMPIERE_FOLDER/org.idempiere.parent/pom.xml"; else break; fi;;
 	esac
 	done
@@ -22,20 +22,40 @@ configure () {
 
 	# Ask for LBR folder
 	while true; do
-	read -e -p "Select the absolute location of LBR project [$PWD/../org.kenos.idempiere.lbr-6.2]: " LBR_FOLDER
-	case $LBR_FOLDER in
-		"" ) if [ ! -f "$PWD/../org.kenos.idempiere.lbr-6.2/org.kenos.idempiere.lbr.p2/target/repository/content.jar" ]; then echo "Error:	Features not found => $LBR_FOLDER/org.kenos.idempiere.lbr.p2/target/repository/"; else LBR_FOLDER="$PWD/../org.kenos.idempiere.lbr-6.2"; break; fi;;
-		* ) if [ ! -f "$LBR_FOLDER/org.kenos.idempiere.lbr.p2/target/repository/content.jar" ]; then echo "Error:	Features not found => $LBR_FOLDER/org.kenos.idempiere.lbr.p2/target/repository/"; else break; fi;;
+	read -e -p "Select the absolute location of upstrem project (LBR or KenosERP) [$PWD/../org.kenos.idempiere.lbr]: " UPSTREAM_FOLDER
+	case $UPSTREAM_FOLDER in
+		"" ) if [ ! -f "$PWD/../org.kenos.idempiere.lbr/pom.xml" ]; then echo "Error:	Features not found => $UPSTREAM_FOLDER/pom.xml"; else UPSTREAM_FOLDER="$PWD/../org.kenos.idempiere.lbr"; break; fi;;
+		* ) if [ ! -f "$UPSTREAM_FOLDER/pom.xml" ]; then echo "Error:	Features not found => $UPSTREAM_FOLDER/pom.xml"; else break; fi;;
 	esac
 	done
 	
-	if [[ ! $LBR_FOLDER == \/* ]]
+	if [[ ! $UPSTREAM_FOLDER == \/* ]]
 	then
 		echo
 		echo "ERROR: Path must be absolute from root /, do not use ~, ./, or ../"
 		echo
 		exit 1
 	fi
+
+	# Ask for Extra Plugins folder
+	while true; do
+	read -e -p "Select the absolute location of Extra Plugins [$PWD/..]: " EXTRA_FOLDER
+	case $EXTRA_FOLDER in
+		"" ) EXTRA_FOLDER="$PWD/.."; break;;
+		* ) break;;
+	esac
+	done
+	
+	if [[ ! $EXTRA_FOLDER == \/* ]]
+	then
+		echo
+		echo "ERROR: Path must be absolute from root /, do not use ~, ./, or ../"
+		echo
+		exit 1
+	fi
+	
+	# Copy template
+	cp pom.xml.template pom.xml
 	
 	common_part=$PWD
 	back=
@@ -47,26 +67,41 @@ configure () {
 	echo
 	echo "Updating pom.xml ... done"
 	CURRENT_DIR="$(echo ${back}${IDEMPIERE_FOLDER#$common_part/} | sed 's/\//\\\//g' | sed 's/ /\\ /g')"
-	COMMAND="sed 's/\${IDEMPIERE-FOLDER}/$CURRENT_DIR/g' pom.xml.template > pom.xml.tmp"
+	COMMAND="sed -i.bak 's/\${IDEMPIERE-FOLDER}/$CURRENT_DIR/g' pom.xml"
 	
 	# Change path on pom.xml
 	eval $COMMAND
 	
 	common_part=$PWD
 	back=
-	while [ "${LBR_FOLDER#$common_part}" = "${LBR_FOLDER}" ]; do
+	while [ "${UPSTREAM_FOLDER#$common_part}" = "${UPSTREAM_FOLDER}" ]; do
 	  common_part=$(dirname $common_part)
 	  back="../${back}"
 	done
 	
 	echo
 	echo "Updating #1 pom.xml ... done"
-	CURRENT_DIR="$(echo ${back}${LBR_FOLDER#$common_part/} | sed 's/\//\\\//g' | sed 's/ /\\ /g')"
-	COMMAND="sed 's/\${LBR-FOLDER}/$CURRENT_DIR/g' pom.xml.tmp > pom.xml"
+	CURRENT_DIR="$(echo ${back}${UPSTREAM_FOLDER#$common_part/} | sed 's/\//\\\//g' | sed 's/ /\\ /g')"
+	COMMAND="sed -i.bak 's/\${UPSTREAM-FOLDER}/$CURRENT_DIR/g' pom.xml"
 	
 	# Change path on pom.xml
 	eval $COMMAND
-	rm pom.xml.tmp
+	
+	common_part=$PWD
+	back=
+	while [ "${EXTRA_FOLDER#$common_part}" = "${EXTRA_FOLDER}" ]; do
+	  common_part=$(dirname $common_part)
+	  back="../${back}"
+	done
+	
+	echo
+	echo "Updating #2 pom.xml ... done"
+	CURRENT_DIR="$(echo ${back}${EXTRA_FOLDER#$common_part/} | sed 's/\//\\\//g' | sed 's/ /\\ /g')"
+	COMMAND="sed -i.bak 's/\${EXTRA-FOLDER}/$CURRENT_DIR/g' pom.xml"
+	
+	# Change path on pom.xml
+	eval $COMMAND
+	rm pom.xml.bak
 	echo
 }
 
@@ -83,15 +118,22 @@ echo	 \(www.kenos.com.br\)
 echo ===================================
 echo 
 
-# Ask for iDempiere folder
-while true; do
-read -t 20 -e -p "Configure iDempiere and LBR folders on pom.xml and target-plarform? [N]: " yn
-case $yn in
-    ""  ) build $*; break;;
-	[Yy]* ) configure; build $*; break;;
-	[Nn]* ) build $*; break;;
-	* ) echo "Please select a valid option. Type Y or N.";;
-esac
-done
+if [ -f "pom.xml" ]
+then
+	# Ask for iDempiere folder
+	while true; do
+	read -t 20 -e -p "Configure iDempiere and LBR folders on pom.xml and target-plarform? [N]: " yn
+	case $yn in
+	    ""  ) build $*; break;;
+		[Yy]* ) configure; build $*; break;;
+		[Nn]* ) build $*; break;;
+		* ) echo "Please select a valid option. Type Y or N.";;
+	esac
+	done
+	
+# No pom.xml found, configure is mandatory	
+else
+	configure; build $*
+fi
 
 exit 0
